ec9e3acc2efbba6b6d099314f568385ec543163b
Shike-Cheng <69415470+Shike-Cheng@users.noreply.github.com>
2024-11-14 23:36:58 +0800

    Update make_benign_database.py

--- CADETS_E3/make_benign_database.py
+++ CADETS_E3/make_benign_database.py
@@ -31,8 +31,8 @@ def main(names=("sourceId", "sourceType", "destinationId", "destinationType", "s
     print("generating freq db")
     freqDict, setOfsets, nodes_sum = createFreqDict(dfList_train, graphNames_train, id_map, 6)
     print(len(setOfsets))
-    writeToFile(freqDict, train_kname + '_freqList.data')   # 这里面记录的是每个src和dst交互的次数
-    writeToFile(setOfsets, train_kname + '_setOfsets.data')  # 记录所有实体的msg
+    writeToFile(freqDict, train_kname + '_freqList.data')
+    writeToFile(setOfsets, train_kname + '_setOfsets.data')
     model, paths_vectors = find_common_string(setOfsets, nodes_sum)
     model.save(train_kname + "_word2vec_model.model")
     np.save(train_kname + "_path_vectors.npy", paths_vectors)
